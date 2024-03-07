c01acf41d329698db02f7578bf29377cef254138
Gary <gary@gtolson.com>
2024-03-06 22:08:45 -0600

    add rooms, items, user choice set

--- python_cli_rpg.py
+++ python_cli_rpg.py
@@ -13,13 +13,13 @@
 # X If they don't have the sword, then they will be eaten by the dragon and lose the game.
 
 #201
-#Save the user input options you allow, e.g., in a set that you can check against when your user makes a choice.
+# X Save the user input options you allow, e.g., in a set that you can check against when your user makes a choice.
 # X Create an inventory for your player, where they can add and remove items. X
-#Players should be able to collect items they find in rooms and add them to their inventory.
-#If they lose a fight against the dragon, then they should lose their inventory items.
-#Add more rooms to your game and allow your player to explore.
-#Some rooms can be empty, others can contain items, and yet others can contain an opponent.
-#Implement some logic that decides whether or not your player can beat the opponent depending on what items they have in their inventory
+# X Players should be able to collect items they find in rooms and add them to their inventory.
+# X If they lose a fight against the dragon, then they should lose their inventory items.
+# X Add more rooms to your game and allow your player to explore.
+# X Some rooms can be empty, others can contain items, and yet others can contain an opponent.
+# X Implement some logic that decides whether or not your player can beat the opponent depending on what items they have in their inventory
 # X Use the random module to add a multiplier to your battles, similar to a dice roll in a real game. This pseudo-random element can have an effect on whether your player wins or loses when battling an opponent.
 
 
@@ -31,12 +31,14 @@ os.system('clear')
 username = input("Please enter your name: ")
 welcome_message = f"Welcome {username} to the world of Gary's mind, lets start to play!"
 dragon_name = "AssHat"
-sword_in_hand = "n"
-empty_room_stay = None
+sword_item = "n"
+room3_stay = None
 dragon_fight_q = None
 item_inventory = []
+user_choices_set = set()
 dragon_health = 20
 player_health = 20
+play_again = None
 
 victory = f"""
                                   ___________
@@ -67,10 +69,10 @@ victory = f"""
     
 def validate_yn():
     print("Please try again and make sure to choose either \"Y\" or \"N\"" )
-    input("Press any \"Enter\" to continue: ")
+    input("Press \"Enter\" to continue: ")
     
 def press_to_continue():
-    input("Press any \"Enter\" to continue: ")
+    input("Press \"Enter\" to continue: ")
 
 
 os.system('clear')
@@ -78,59 +80,118 @@ print(f"{welcome_message}", end='\n\n')
 
 while username != None:
     os.system('clear')
-    print(f"{username}, You are in the front room of a large castle, there are 2 doors in front of you. " 
-          "One to the right and one to the left. ")
-    print("Choose the door on the right by typing \"R\" or choose the door on the left by typing \"L\" "
+    print(f"{username}, You are in the front room of a large castle, there are 4 doors in front of you. " 
+          "Each door had a number 1,2,3 or 4. ")
+    print("Choose the door #1 by typing \"1\" door #2 by typing \"2\" etc... "
           "or type \"E\" to exit the game", end='\n\n')
-    frontroom_door = input("Make your choice (L/R/E): ")
+    frontroom_door = input("Make your choice (1-4 or E to exit): ")
     frontroom_door = frontroom_door.lower()
-    if (frontroom_door != "l") and (frontroom_door != "r") and (frontroom_door != "e"):
-        os.system('clear')
-        print("Please try again and make sure to choose either \"L\" or \"R\" or \"E\"")
-        press_to_continue()
-        continue
-    
     if frontroom_door == 'e':
         print("Thank you for playing come back if you dare!")
         exit()
         
-    while frontroom_door == "l":
+    if frontroom_door.isalpha():
         os.system('clear')
+        print("Please try again and make sure to choose either \"1-4\" or \"E\"")
+        press_to_continue()
+        continue
+    
+    frontroom_door = int(frontroom_door)
+    if frontroom_door not in range(1,4):
+        os.system('clear')
+        print("Please try again and make sure to choose either \"1-4\" or \"E\"")
+        press_to_continue()
+        continue
+
+    while frontroom_door == 1:
+        os.system('clear')
+        if 1 in user_choices_set:
+            print("You have already been to this room...")   
+        rm1_chest = input("The door opens and you find a chest.. Would you like to open it? (Y/N) ")
+        rm1_chest = rm1_chest.lower()
+        if rm1_chest != 'y' and rm1_chest != 'n':
+            os.system('clear')
+            validate_yn()
+            continue
+        if rm1_chest == 'y':
+            user_choices_set.add("room1_chest")
+            print("In the chest you have found a key that you need, very good! It has been added to your inventory")
+            item_inventory.append("skel_key")
+            print("The key is the only worth while thing in this room.. guiding you back to the frontroom")
+        else:
+            print("You chose not to open the chest to see whats inside.. sending you back to the frontroom.")           
+        press_to_continue()
+        break
+
+    while frontroom_door == 2:
+        os.system('clear')
+        if 2 in user_choices_set:
+            print("You have already been to this room...")
+        rm2_unlock = input("This door is locked. Do you have a key to open it? (Y/N) ")
+        rm2_unlock = rm2_unlock.lower()
+        if rm2_unlock != "y" and rm2_unlock != "n":
+            os.system('clear')
+            validate_yn()
+            continue
+        if "skel_key" in item_inventory:
+            print("You do have the skeleton key and it has worked to open the door...")
+            club_item = input("Inside you find a club, would you like to pick it up? (Y/N)")
+            if club_item != 'y' and club_item != 'n':
+                os.system('clear')
+                validate_yn()
+                continue
+            if club_item == "y":
+                item_inventory.append("club")
+                print("Great you have added the club to your item inventory... you may need it.")
+                print("The club is all that is useful here.. guiding you back to the frontroom")
+            else:
+                print("You have chosen not to take the club, there is not much else here...")
+                print("Guiding you back to the frontroom.")
+        else:
+            print("You dont have a key to open the door... guiding you back to the frontroom...")
+        press_to_continue()
+        break
+
+
+    while frontroom_door == 3:
+        os.system('clear')
+        if 3 in user_choices_set:
+            print("You have already been to this room...")
         print("You have come to what appears to be an empty room.", end='\n\n')
-        empty_room_stay = input("Would you like to stay and look around in this empty room? (Y/N)")
-        empty_room_stay = empty_room_stay.lower()
-        if empty_room_stay != "y" and empty_room_stay != "n":
+        room3_stay = input("Would you like to stay and look around in this empty room? (Y/N)")
+        room3_stay = room3_stay.lower()
+        if room3_stay != "y" and room3_stay != "n":
             os.system('clear')
             validate_yn()
             continue
         break
     
-    if empty_room_stay == "n":
+    if room3_stay == "n":
         print("You have chosen not to stay, guiding you back to the front room: ")
         press_to_continue()
-        empty_room_stay = None
+        room3_stay = None
         continue
     
-    if empty_room_stay == "y":
+    if room3_stay == "y":
         os.system('clear')
         print("Good for you in your looking around you found a sword, you really need a weapon.", end='\n\n')
-        while empty_room_stay == "y":
-            sword_in_hand = input("Would you like to pick up the sword (Y/N): ")
-            sword_in_hand = sword_in_hand.lower()
-            if sword_in_hand != "y" and sword_in_hand != "n":
+        while room3_stay == "y":
+            sword_item = input("Would you like to pick up the sword (Y/N): ")
+            sword_item = sword_item.lower()
+            if sword_item != "y" and sword_item != "n":
                 validate_yn()
                 continue
             else:
-                if sword_in_hand == "y":
+                if sword_item == "y":
                     item_inventory.append("sword")
-                empty_room_stay = None
+                room3_stay = None
                 break
             
         if "sword" in item_inventory:
             os.system('clear')
             print("Congrats.., you now have a sword! You are armed and ready "
                   "to fight whatever comes your way. ")
-            print("You have found eveything here. We will guide you back to the front room. once there try the door on the Right.", end='\n\n')
+            print("You have found eveything here. We will guide you back to the front room. once there try one of the other doors.", end='\n\n')
 
         if "sword" not in item_inventory:
             os.system('clear')
@@ -141,8 +202,10 @@ while username != None:
         press_to_continue()
         continue
             
-    while frontroom_door == "r":
+    while frontroom_door == 4:
         os.system('clear')
+        if 4 in user_choices_set:
+            print("You have already been to this room...")
         print(f"In this room you meet the dragon named {dragon_name}, That's his name because he's a jerk.", end='\n\n')
         if "sword" in item_inventory:
             print(f"You have a sword and if you know how to use it, there are great treasures "
@@ -160,7 +223,10 @@ while username != None:
             validate_yn()
             continue
         break
-            
+    
+    # add front room door to location history set for later lookup
+    user_choices_set.add(frontroom_door)
+    
     if dragon_fight_q == "n":
         os.system('clear')
         if "sword" in item_inventory:
@@ -177,7 +243,6 @@ while username != None:
         os.system('clear')
         print(f"Ok its on... best of luck! {dragon_name} is really big and does not like visitors! "
               "Fight brave human...")
-        #time.sleep(10)
         if "sword" not in item_inventory:
             print("Because you dont have a weapon you will need to roll a 19 or better to score a hit.")
             while player_health > 0 and dragon_health > 0:
@@ -196,8 +261,22 @@ while username != None:
                 os.system('clear') 
                 print(f"{dragon_name} burned you alive, then ate you, waited an hour then pooped you out "
                     "so he could use you as compost for his collection of rare flowers. "
-                    "(We told you he was a jerk...) ")
-                exit()
+                    "(We told you he was a jerk...) \n\n")
+                print(f"Thus ends your quest... You may play again, however you have lost all the items you have collected.")
+                while  play_again != 'y' and play_again != 'n':
+                    play_again = input("Would you like to play again? (Y/N) ")
+                    play_again = play_again.lower()
+                    if play_again != 'y' and play_again != 'n':
+                        validate_yn()
+                        continue
+                if play_again == "y":
+                    item_inventory = []
+                    dragon_health = 20
+                    player_health = 20
+                    play_again = None
+                    continue
+                if play_again == "n":
+                    exit()
             if dragon_health <=0:
                 os.system('clear') 
                 print(victory)
@@ -212,24 +291,38 @@ while username != None:
                 print(f"battle_roll = {roll20}")
                 roll12 = random.randint(1, 12)
                 print(f"damage_roll = {roll12}")
-                if roll20 >= 5:
+                if roll20 >= 19:
                     dragon_health -= roll12
                     print(f"You scored a hit: {dragon_name} has {dragon_health} points remaning")
-                elif roll20 < 5:
+                elif roll20 < 19:
                     player_health -= roll12
                     print(f"Ouch! {dragon_name} got you.. you have {player_health} points remaning") 
-                if player_health <= 0:
-                    os.system('clear')
-                    print(f"{dragon_name} burned you alive, then ate you, waited an hour then pooped you out "
-                        "so he could use you as compost for his collection of rare flowers. "
-                        "(We told you he was a jerk...) ")
+            if player_health <= 0:
+                os.system('clear')
+                print(f"{dragon_name} burned you alive, then ate you, waited an hour then pooped you out "
+                    "so he could use you as compost for his collection of rare flowers. "
+                    "(We told you he was a jerk...) ")
+                print(f"Thus ends your quest... You may play again, however you have lost all the items you have collected.")
+                while  play_again != 'y' and play_again != 'n':
+                    play_again = input("Would you like to play again? (Y/N) ")
+                    play_again = play_again.lower()
+                    if play_again != 'y' and play_again != 'n':
+                        validate_yn()
+                        continue
+                if play_again == "y":
+                    item_inventory = []
+                    dragon_health = 20
+                    player_health = 20
+                    play_again = None
+                    continue
+                if play_again == "n":
                     exit()
-                if dragon_health <=0:
-                    os.system('clear')            
-                    print(victory)
-                    print(f"Wow you sure are good with that sword! You killed {dragon_name}!!!! "
-                          f"To the victor come the spoils!!! All of {dragon_name}'s treasure is yours")
-                    exit()
+            if dragon_health <=0:
+                os.system('clear')            
+                print(victory)
+                print(f"Wow you sure are good with that sword! You killed {dragon_name}!!!! "
+                      f"To the victor come the spoils!!! All of {dragon_name}'s treasure is yours")
+                exit()
     
         
         
